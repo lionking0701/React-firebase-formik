9bd096da0cf6bf3e5f3f4fe7d02fbe10ace6de35
Lionking0701 <taiyingangel0701@gmail.com>
2018-11-07 10:08:08 -0500

    add posts

---
+++ src/components/Posts/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Posts extends React.Component {
+  render() {
+    return (
+      <div>this is posts index</div>
+    )
+  }
+}
+
+export default Posts
--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter, Route } from 'react-router-dom'
-import App from 'components/App'
 import Root from 'Root'
+import App from 'components/App'
+import Posts from 'components/Posts'
 
 ReactDOM.render(
   <Root>
     <BrowserRouter>
       <Route path="/" component={App} />
+      <Route path="/posts" component={Posts} />
     </BrowserRouter>
   </Root>
   ,
