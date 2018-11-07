a473452d1260fac9f145dfc5f8d7824ca0db7fa5
Lionking0701 <taiyingangel0701@gmail.com>
2018-11-07 10:36:38 -0500

    add home

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
+import { Route } from 'react-router-dom'
+import Home from 'components/Home'
+import Posts from 'components/Posts'
 
 class App extends React.Component {
   render() {
     return (
-      <div>this is the app</div>
+      <div>
+        <Route exact path="/" component={Home} />
+        <Route path="/posts" component={Posts} />
+      </div>
     )
   }
 }
