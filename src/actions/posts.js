9ac902f28a9ba45f1423f6d157b24b6461952c32
Lionking0701 <taiyingangel0701@gmail.com>
2018-11-15 10:43:43 -0500

    add posts index component

--- src/actions/posts.js
+++ src/actions/posts.js
@@ -2,12 +2,15 @@ import * as types from 'actions/types'
 import firestore from 'utils/firebase/firestore'
 
 export const fetchPosts = () => dispatch => {
-  firestore.collection("posts").get().then((querySnapshot) => {
+  const posts = []
+  return firestore.collection("posts").get().then((querySnapshot) => {
     querySnapshot.forEach((doc) => {
-        console.log(`${doc.id} => ${doc.data()}`)
+      let data = doc.data()
+      let { title, body } = data
+      posts.push({ id: doc.id, title: title, body: body })
     })
+    dispatch(setPosts(posts))
   })
-  dispatch(setPosts([]))
 }
 
 export const setPosts = (posts) => ({
